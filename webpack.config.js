ba04cb39a0381569de84e6d3edc6028c7e75e6de
sakhura <sabina.romero@gmail.com>
2026-02-12 22:46:40 -0300

    se quitan duplicados

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const path = require('path');
 
 module.exports = {
@@ -9,16 +9,4 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
     },
     devtool: 'inline-source-map'
-=======
-const path = require('path');
-
-module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devtool: 'inline-source-map'
->>>>>>> 7ca411eb40d1ea326b40e7598e2cbf65e9d7765b
 };
